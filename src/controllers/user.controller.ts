b3c795bd7875151b25f27ee52638cc8240fb3c62
Mauro Hernandez <mauroDeveloper@hotmail.com>
2023-11-23 13:39:21 -0400

    Remove dependency injection

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,11 @@
 import { Router } from 'express';
-import { Service, Inject } from 'typedi';
 import { USER, USER_DETAILS, USER_REGISTER, USER_UPDATE } from '@Urls/user.url';
 import { commonController } from '@Utils/commonController';
-import UserService from '@Services/user.service';
 
-@Service()
 class UserController {
-  @Inject()
-  private readonly userService: UserService;
-  
-  //
   private readonly controller: Router;
 
-  constructor(private readonly router: Router) {
+  constructor(private router: Router) {
     this.controller = Router();
   }
 
@@ -21,9 +14,7 @@ class UserController {
     this.controller.get(
       USER_DETAILS,
       commonController(async (req, res, next) => {
-        console.log(this.userService)
-        this.userService.getDetails("9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d");
-        return 'true';
+        return '[GET] obtain a users information';
       })
     );
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import 'reflect-metadata';
 import '@Config/loadEnv';
 import './App';
